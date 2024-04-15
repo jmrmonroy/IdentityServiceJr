d40b2b8b0d08880796a8ac52da793c9866594fa5
ProjectSupport <you@example.com>
2024-04-15 17:53:25 -0500

    Primer commit

---
+++ src/main/java/co/com/identityservice/identitybackendservice/IdentitybackendServiceApplication.java
@@ -0,0 +1,13 @@
+package co.com.identityservice.identitybackendservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentitybackendServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentitybackendServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/identityservice/identitybackendservice/IdentitybackendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.identityservice.identitybackendservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentitybackendServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
